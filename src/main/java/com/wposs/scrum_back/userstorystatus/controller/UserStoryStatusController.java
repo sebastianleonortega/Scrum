2b486f4ef6dad36b64bed34f65147746d369aef4
Bankend Yorman S <sanchezyorman0@gmail.com>
2023-04-14 16:36:27 -0500

    CREANDO MODULO DE STATUS DE HISTORIAS DE USUARIOS V1.4

--- src/main/java/com/wposs/scrum_back/userstorystatus/controller/UserStoryStatusController.java
+++ src/main/java/com/wposs/scrum_back/userstorystatus/controller/UserStoryStatusController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("userstorystatus")
+@RequestMapping("/userstorystatus")
 public class UserStoryStatusController {
     @Autowired
     private UserStoryStatusService userStoryStatusService;
