f20375e7a334c5ba5aceaf85a22dcc7308f4fe2f
sebastian <jsleono@ufpso.edu.co>
2023-04-22 11:03:21 -0500

    v1 22/04/2023 11:00 Am Hu

--- src/app/modules/subprojects/pages/user-story-management/user-story-management.component.ts
+++ src/app/modules/subprojects/pages/user-story-management/user-story-management.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
 import { User_storyService } from "@app/data/services/user_story/user_story.service";
 import { ActivatedRoute } from "@angular/router";
+import { UserStoyStatusService } from '@app/data/services/user-story-status/user-stoy-status.service';
 
 
 @Component({
@@ -10,35 +11,48 @@ import { ActivatedRoute } from "@angular/router";
   styleUrls: ['./user-story-management.component.css']
 })
 export class UserStoryManagementComponent implements OnInit {
+  userStoryForm: FormGroup = new FormGroup({});
   userStory: any;
   id: any;
-  userStoryForm: FormGroup = new FormGroup({});
+  userStoryState;
+
 
   constructor(
-    public formBuilder: FormBuilder,
-    public userStoryService: User_storyService,
-    private route: ActivatedRoute) {
+    private formBuilder: FormBuilder,
+    private userStoryService: User_storyService,
+    private route: ActivatedRoute,
+    private userStoryStateService: UserStoyStatusService,
+    ){
   }
 
   ngOnInit(): void {
     this.userStoryForm = this.formBuilder.group({
-      subProjectId: new FormControl(null, [Validators.required]),
       userStoryName: new FormControl(null, [Validators.required]),
       userStoryArchive: new FormControl(null, []),
       userStoryStateId: new FormControl(null, [Validators.required]),
+      userStoryScore: new FormControl(null, [Validators.required]),
+      fechaMaxima: new FormControl(null, [Validators.required])
     });
 
     this.id = this.route.snapshot.paramMap.get('userStoryId');
     this.getUserStoryById(this.id)
+    this.getUserStoryState()
+  }
+
+  getUserStoryState(){
+    this.userStoryStateService.getAlluser_story_status().subscribe(resp =>{
+      this.userStoryState = resp;
+    })
   }
 
   getUserStoryById(id: string | null) {
     this.userStoryService.getUserStoryById(id).subscribe(resp => {
         this.userStory = resp;
         this.userStoryForm.patchValue({
-          subProjectId: this.userStory.subProjectId,
           userStoryName: this.userStory.userStoryName,
           userStoryStateId: this.userStory.userStoryStateId,
+          userStoryScore: this.userStory.userStoryScore,
+          fechaMaxima: this.userStory.fechaMaxima,
         });
       },
     );
@@ -47,10 +61,11 @@ export class UserStoryManagementComponent implements OnInit {
   edit() {
     if (this.userStoryForm.valid) {
       const data = {
-        subProjectId: this.userStoryForm.get('subProjectId')?.value,
         userStoryArchive: this.userStoryForm.get('userStoryArchive')?.value,
         userStoryName: this.userStoryForm.get('userStoryName')?.value,
-        userStoryStateId: this.userStoryForm.get('userStoryStateId')?.value
+        userStoryStateId: this.userStoryForm.get('userStoryStateId')?.value,
+        userStoryScore: this.userStoryForm.get('userStoryScore')?.value,
+        fechaMaxima: this.userStoryForm.get('fechaMaxima')?.value
       }
       this.userStoryService.updateUserStory(this.id, data).subscribe(
         (resp) => {
