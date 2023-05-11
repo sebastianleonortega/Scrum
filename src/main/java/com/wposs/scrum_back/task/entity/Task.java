aadc6aab7ed4e274cd23cd998f6c0a58b1054459
Bankend Yorman S <sanchezyorman0@gmail.com>
2023-05-11 09:02:03 -0500

    Implementacion de Task v3.1 2023/05/11

---
+++ src/main/java/com/wposs/scrum_back/task/entity/Task.java
@@ -0,0 +1,34 @@
+package com.wposs.scrum_back.task.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "task", schema = "wposs")
+public class Task {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID",strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "id_task")
+    private UUID taskId;
+    @Column(name = "name_task",nullable = false,length = 30,unique = true)
+    private String nameTask;
+
+    public UUID getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(UUID taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getNameTask() {
+        return nameTask;
+    }
+
+    public void setNameTask(String nameTask) {
+        this.nameTask = nameTask;
+    }
+}
