fbb8add248e336ce3c0543772cacdb89c5b5c587
Bankend Yorman S <sanchezyorman0@gmail.com>
2023-05-11 10:46:17 -0500

    Implementacion de Observation v3.2.1 2023/05/11

--- src/main/java/com/wposs/scrum_back/observation/entity/Observation.java
+++ src/main/java/com/wposs/scrum_back/observation/entity/Observation.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 import java.util.UUID;
 
 @Entity
-@Table(name = "observation")
+@Table(name = "observation",schema = "wposs")
 public class Observation {
     @Id
     @GeneratedValue(generator = "UUID")
